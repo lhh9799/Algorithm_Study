ab51aa78a262a3c3508e2a6d58998e95a3fa9f52
Hyunho Lee <77262005+lhh9799@users.noreply.github.com>
2023-09-25 21:07:22 +0900

    [Gold IV] Title: 연구소, Time: 440 ms, Memory: 70788 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,113 +1,135 @@
-import java.io.BufferedInputStream;
-import java.util.Scanner;
-
-import javax.imageio.ImageTypeSpecifier;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N , M, result , max = 0 ;
-	static int[][] arr , arr1;
 	
-	static boolean[][] isVisited;
+	static int N, M;
+	static int[][] origin;
+	static int[][] copy;
+	static ArrayList<int[]> blank = new ArrayList<>();
+	static ArrayList<int[]> virus = new ArrayList<>();
+	static ArrayDeque<int[]> virusQueue = new ArrayDeque<>();
+	static int[] selected = new int[3];
+	static int[][] directions = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+	static int answer = Integer.MIN_VALUE;
 	
-	static int[] dr = {1,-1,0,0};
-	static int[] dc = {0,0,-1,1};
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static void copyArray(final int[][] origin, int[][] dest) {
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				dest[i][j] = origin[i][j];
+			}
+		}
+	}
+	
+	static void simulate() {
+		copyArray(origin, copy);
 		
-		 N = sc.nextInt();
-		 M = sc.nextInt();
-		arr = new int[N][M];
-		arr1 = new int[N][M];
+		for(int i=0; i<selected.length; i++) {
+			int[] newWallPosition = blank.get(selected[i]);
+			
+			copy[newWallPosition[0]][newWallPosition[1]] = 1;
+		}
 		
-		for(int i = 0 ; i < N ;i++) {
-			for(int j = 0 ; j < M ; j++) {
-				arr[i][j] = sc.nextInt();
+		ArrayDeque<int[]> queue = virusQueue.clone();
+		
+		while(!queue.isEmpty()) {
+			int[] startPoint = queue.poll();
+			int startX = startPoint[0];
+			int startY = startPoint[1];
+			
+			for(int j=0; j<directions.length; j++) {
+				int x = startX + directions[j][0];
+				int y = startY + directions[j][1];
+				
+				if(x >= 0 && x < N && y >= 0 && y < M && copy[x][y] == 0) {
+					copy[x][y] = 2;
+					
+					queue.offer(new int[] {x, y});
+				}
+				
 			}
 		}
 		
 		
-		for(int i =0 ; i < N*M ; i++) {
-			for(int j = i+1 ; j < N*M ; j++ ) {
-				for(int k = j+1 ; k < N*M ;k++) {
-					
-					
-					
-					 for(int r = 0 ; r < N ;r++) {
-						 for(int c = 0 ; c < M ; c++ ) {
-							 arr1[r][c] = arr[r][c];
-						 }
-					 }
-					
-					if(arr1[i/M][i%M] != 0)
-						continue;
-					
-					if(arr1[j/M][j%M] != 0)
-						continue;
-					
-					if(arr1[k/M][k%M] != 0 )
-						continue;
-					
-					arr1[i/M][i%M] = 1 ;
-					
-					arr1[j/M][j%M] = 1;
-					
-					arr1[k/M][k%M] = 1;
-					
-					isVisited = new boolean[N][M];
-					
-					
-					for(int r = 0 ; r < N ;r++) {
-						for(int c = 0 ; c < M ;c++) {
-							if(arr1[r][c] ==2)
-								
-								dfs(r,c);	
-						}
-					}
-					
-					result = 0 ;
-					for(int r = 0 ; r < N ;r++) {
-						for(int c = 0 ; c < M ;c++) {
-							if(arr1[r][c]==0 && !isVisited[r][c])
-								result++;
-						}
-					}
-					
-					
-					max = Math.max(max, result);
-					
+		for(int i=0; i<virus.size(); i++) {
+			int[] startPoint = virus.get(i);
+			
+			int x = startPoint[0];
+			int y = startPoint[1];
+			
+			for(int j=0; j<directions.length; j++) {
+				x += directions[j][0];
+				y += directions[j][1];
+				
+				while(x >= 0 && x < N && y >= 0 && y < M && copy[x][y] == 0) {
+					copy[x][y] = 2;
 				}
 			}
 		}
 		
-		System.out.println(max);
-		
+		countSafeZone();
 	}
 	
+	static void combination(int start, int count) {
+		if(count == 3) {
+			simulate();
+			
+			return;
+		}
+		
+		for(int i=start; i<blank.size(); i++) {
+			selected[count] = i;
+			
+			combination(i+1, count+1);
+		}
+	}
 	
-	public static void dfs(int r , int c ) {
+	static void countSafeZone() {
+		int count = 0;
 		
-		isVisited[r][c] = true;
-		
-		for(int i =0 ; i< 4; i++) {
-			
-			int dR = r+ dr[i];
-			int dC = c+ dc[i];
-			
-			if(isRangeCheck(dR, dC) && arr1[dR][dC] == 0 && !isVisited[dR][dC] ) {
-				//arr1[r][c] = 3;
-				dfs(dR, dC);
-				
-				
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				if(copy[i][j] == 0) {
+					count++;
+				}
 			}
 		}
 		
+		answer = Math.max(answer, count);
 	}
-	
-	public static boolean isRangeCheck( int r, int c) {
-		if(r >=N || r < 0 || c>=M || c <0 )
-			return false;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
 		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		origin = new int[N][M];
+		copy = new int[N][M];
 		
-		return true;
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			for(int j=0; j<M; j++) {
+				origin[i][j] = Integer.parseInt(st.nextToken());
+				
+				if(origin[i][j] == 0) {				//빈 칸
+					blank.add(new int[] {i, j});
+				}
+				else if(origin[i][j] == 2) {		//바이러스
+					virus.add(new int[] {i, j});
+					virusQueue.add(new int[] {i, j});
+				}
+			}
+		}
+		
+		combination(0, 0);
+		System.out.println(answer);
 	}
+
 }
