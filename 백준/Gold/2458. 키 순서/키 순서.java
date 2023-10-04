e628bf83464e2a754f02b714bcc0a56ca4338350
Hyunho Lee <77262005+lhh9799@users.noreply.github.com>
2023-10-04 23:26:25 +0900

    [Gold IV] Title: 키 순서, Time: 728 ms, Memory: 40008 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,68 +1,84 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	Scanner sc =new Scanner(System.in);
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		ArrayDeque<Integer> queue = new ArrayDeque<>();		//자신보다 키가 작거나 큰 학생들을 탐색하는 큐
+		int a = 0, b = 0;									//a: 키가 작은 학생의 번호, b: 키가 큰 학생의 번호
 		
-			int N = sc.nextInt(); //node 수 입력받기
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());			//학생들의 수 N
+		int M = Integer.parseInt(st.nextToken());			//두 학생의 키를 비교한 횟수 M
+		int[][] map = new int[N][N];						//학생들의 키 상관관계
+		int[] correlation = new int[N];						//진출차수 + 진입차수를 저장하는 배열
+		boolean[] visited = new boolean[N];					//방문 배열
+		int answer = 0;
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
 			
-			// 이차원 배열 생성
+			a = Integer.parseInt(st.nextToken()) - 1;		//키가 작은 학생의 번호 (0부터 시작)
+			b = Integer.parseInt(st.nextToken()) - 1;		//키가 큰 학생의 번호 (0부터 시작)
 			
-			int[][] arr = new int[N][N];
+			map[a][b] = 1;									//키가 작은 학생 -> 키가 큰 학생: -1
+			map[b][a] = -1;									//키가 큰 학생 -> 키가 작은 학생: 1
+		}
+		
+		//자신보다 키가 큰 학생의 수 BFS 탐색
+		for(int i=0; i<N; i++) {
+			queue.clear();									//큐 초기화
+			queue.add(i);									//자기 자신 큐에 삽입
+			Arrays.fill(visited, false);					//방문 배열 초기화
 			
-			
-			//배열값 입력받기 [r][c] = r 에서 시작해서 c 까지의 거리
-			for(int r = 0 ; r < N ; r++) {
-				for(int c = 0 ; c< N ;c ++) {			
-					if(r==c ) continue;	// 자기자신 부터 자기 자신까지의 거리는 0입니다..
-					
-					// [r][c]= 0 이다? 아직 최단 경로를 모른다 
-					// 플루이드 워셜로 최단 경로 구해줘야하기에 적당히 큰 수 100000으로 설정
-					if(arr[r][c] == 0)
-					arr[r][c] = 100000;
-				}
-			}
-			
-			int M = sc.nextInt();
-			
-			for(int i = 0 ; i < M ;i++) {
-				arr[sc.nextInt()-1][sc.nextInt()-1] = 1;
-			}
-			
-			
-			//플루이드 워셜 알고리즘 수행 경출도
-			for(int k = 0 ; k < N ; k++) {
-				for(int i = 0 ; i < N ;i++) {
-					if(i == k) continue; // 경로와 출발값이 같다면 continue;
-					for(int j = 0 ; j < N ;j++) {
-						// 거쳐오는 경로와 도착 점 이 같거나 출발과 도착이 같다면 continue; 
-						if(j == k || j == i) continue; 
-						arr[i][j] = Math.min(arr[i][k]+ arr[k][j], arr[i][j]);
-						
+			while(!queue.isEmpty()) {
+				int start = queue.poll();
+				
+				for(int j=0; j<N; j++) {
+					//큐에서 뺀 원소와 연결되어 있으면서 키가 큰 학생이 있는 경우
+					if(!visited[j] && map[start][j] == 1) {
+						queue.add(j);						//큐에 추가
+						correlation[i]++;					//진출차수 증가
+						visited[j] = true;					//방문한 것으로 표시
 					}
 				}
 			}
-			
-			
-			int result = 0 ;
-			for(int i = 0 ; i < N ; i++) {
-				int count = 0 ;
-				for(int j = 0 ; j < N ;j++) {
-					if(arr[i][j] != 0 && arr[i][j] != 100000) count++;
-					if(arr[j][i] != 0 && arr[j][i] != 100000) count++;
-					
-					
-				}
-				
-				if(count >= N-1) result++;
-			
-			}
-			
-			System.out.println(result);
-			
 		}
-	
-
+		
+		//자신보다 키가 작은 학생의 수 BFS 탐색
+		for(int i=0; i<N; i++) {
+			queue.clear();									//큐 초기화
+			queue.add(i);									//자기 자신 큐에 삽입
+			Arrays.fill(visited, false);					//방문 배열 초기화
+			
+			while(!queue.isEmpty()) {
+				int start = queue.poll();
+				
+				for(int j=0; j<N; j++) {
+					//큐에서 뺀 원소와 연결되어 있으면서 키가 작은 학생이 있는 경우
+					if(!visited[j] && map[start][j] == -1) {
+						queue.add(j);						//큐에 추가
+						correlation[i]++;					//진입차수 증가
+						visited[j] = true;					//방문한 것으로 표시
+					}
+				}
+			}
+		}
+		
+		//(자신보다 키가 작은 학생들의 수 + 자신보다 키가 큰 학생들의 수)가 N-1인 학생의 수 카운트
+		for(int i=0; i<N; i++) {
+			if(correlation[i] == N-1) {
+				answer++;
+			}
+		}
+		
+		System.out.println(answer);
 	}
+
+}
