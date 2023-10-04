f44c2fc93af089c1a95955618da012e739bf693d
Hyunho Lee <77262005+lhh9799@users.noreply.github.com>
2023-10-04 17:44:10 +0900

    [Gold III] Title: 다리 만들기, Time: 204 ms, Memory: 16312 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -1,131 +1,123 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] arr ;
-	static boolean[][] isVisited ;
-	static int N;
-	static int[] dr = {1,-1,0,0};
-	static int[] dc = {0,0,1,-1};
-	static int result = Integer.MAX_VALUE;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		
-		
-		arr = new int[N][N] ;
-		isVisited = new boolean[N][N];
-		
-		
-		for(int r = 0 ; r < N ;r ++) {
-			for(int c = 0 ; c < N ;c++) {
-				arr[r][c] = sc.nextInt();
-			}
+	
+	static int N;									//지도의 크기 N
+	static int[][] map;								//지도
+	static boolean[][] visited;						//방문 배열
+	static int answer = Integer.MAX_VALUE;			//가장 짧은 다리의 길이 (최댓값으로 초기화)
+	//상, 하, 좌, 우
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	
+	static void clearVisitedArray() {
+		for(int i=0; i<visited.length; i++) {
+			Arrays.fill(visited[i], false);
 		}
-		
-		int count = 1;
-		
-		for(int r = 0 ; r < N ;r ++) {
-			for(int c = 0 ; c < N ;c++) {
-				if(arr[r][c]==1 && !isVisited[r][c]) {
-				dfs(r,c,count);
-				count++;
-				}
-			}
-		}
-		
-//		for(int r = 0 ; r < N ;r ++) {
-//			for(int c = 0 ; c < N ;c++) {
-//				System.out.print(arr[r][c]);
-//				}
-//			System.out.println();
-//			}
-		
-		for(int r = 0 ; r < N ;r ++) {
-			for(int c = 0 ; c < N ;c++) {
-				if(arr[r][c] != 0) {
-					isVisited = new boolean[N][N];
-					bfs(r,c, arr[r][c]);
-				}
-			}
-		}
-		
-		System.out.println(result);
-		
 	}
 	
-	
-	
-	
-	
-	
-	
-	private static void bfs(int r, int c, int island) {
-		// TODO Auto-generated method stub
+	static void markIslandNumber(int row, int col, int islandNo) {
+		ArrayDeque<int[]> queue = new ArrayDeque<>();
+		clearVisitedArray();
+		int[] currentCoord = null;
+		int adjX = 0, adjY = 0;
 		
-		Queue<int[]> q = new ArrayDeque<int[]>() ;
+		queue.offer(new int[] {row, col});
+		map[row][col] = islandNo;
+		visited[row][col] = true;
 		
-		q.add(new int[] {r,c,island , 0});
-		isVisited[r][c] = true;
-		
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
+		while(!queue.isEmpty()) {
+			currentCoord = queue.poll();
 			
-			for(int i = 0 ; i < 4 ;i++) {
-				int nextR = cur[0] + dr[i];
-				int nextC = cur[1] + dc[i];
+			for(int i=0; i<4; i++) {
+				adjX = currentCoord[0] + dx[i];
+				adjY = currentCoord[1] + dy[i];
 				
-				if(isRange(nextR,nextC) && !isVisited[nextR][nextC] ) {
+				//이번 메소드 호출 때 방문한 적 없으면서 섬 번호가 배정되지 않은 육지(값이 1)인 경우 큐에 추가
+				if(adjX >= 0 && adjX < N && adjY >= 0 && adjY < N && !visited[adjX][adjY] && map[adjX][adjY] == 1) {
+					queue.offer(new int[] {adjX, adjY});
+					map[adjX][adjY] = islandNo;
+					visited[adjX][adjY] = true;
+				}
+			}
+		}
+	}
+	
+	static void findMinBridgeLength(int row, int col) {
+		ArrayDeque<int[]> queue = new ArrayDeque<>();
+		clearVisitedArray();
+		int[] currentCoord = null;
+		int adjX = 0, adjY = 0;
+		int distance = 0;
+		
+		queue.offer(new int[] {row, col});
+		visited[row][col] = true;
+		
+		while(!queue.isEmpty()) {
+			currentCoord = queue.poll();
+			
+			for(int i=0; i<4; i++) {
+				adjX = currentCoord[0] + dx[i];
+				adjY = currentCoord[1] + dy[i];
+				
+				//이번 메소드 호출 때 방문한 적이 없으면
+				if(adjX >= 0 && adjX < N && adjY >= 0 && adjY < N && !visited[adjX][adjY]) {
+					visited[adjX][adjY] = true;
+					distance = Math.abs(adjX - row) + Math.abs(adjY - col) - 1;
 					
-					if(arr[nextR][nextC] == 0 && cur[3] < result) {
-						isVisited[nextR][nextC] = true;
-						q.add(new int[] {nextR, nextC , island , cur[3] +1 });
+					//바다이면 큐에 추가 (육지를 찾을 때 까지 탐색) +이전 최소거리보다 작은 경우 큐에 추가
+					if(map[adjX][adjY] == 0 && distance < answer) {
+						queue.offer(new int[] {adjX, adjY});
 					}
+					//자신의 섬 번호와 다른 섬 번호를 만난 경우 경로 계산
+					else if(map[adjX][adjY] != map[row][col]) {
+						if(distance < answer) {
+							answer = Math.min(answer, distance);
+							
+							break;
+						}
 						
-					
-					
-					if(arr[nextR][nextC] !=0 && arr[nextR][nextC] != island) {
-						result = Math.min(result, cur[3]);
-						break;
 					}
-						
 				}
 			}
 		}
 	}
 
-
-
-
-
-
-
-	private static void dfs(int r, int c , int count) {
-		// TODO Auto-generated method stub
-		isVisited[r][c] = true;
-		arr[r][c] = count;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
 		
-		
-		for(int i = 0 ; i < 4 ; i++) {
-			int nextR = r+ dr[i];
-			int nextC = c + dc[i];
+		N = Integer.parseInt(br.readLine());		//지도의 크기 N
+		map = new int[N][N];						//지도
+		visited = new boolean[N][N];				//방문 배열
+		int islandNo = 2;							//섬의 번호는 2부터 시작 (1: (입력 값) 육지)
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
 			
-			if(isRange(nextR,nextC) && !isVisited[nextR][nextC] && arr[nextR][nextC] == 1) {
-				dfs(nextR, nextC , count);
+			for(int j=0; j<N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-	}
-	
-	private static boolean isRange(int r , int c) {
-		if(r >= N || r < 0 || c >=N || c < 0) 
-			return false;
-			
+		//섬 번호 지정 또는 다리의 길이 계산
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				//1. 입력받은 칸이 1 (육지)이면 섬의 번호 지정
+				if(map[i][j] == 1) {
+					markIslandNumber(i, j, islandNo++);
+				}
+				if(map[i][j] > 0) {
+					findMinBridgeLength(i, j);
+				}
+			}
+		}
 		
-		return true;
+		System.out.println(answer);
 	}
 
 }
