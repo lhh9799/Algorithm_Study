83d1c2af2b6199f42e2a944681fd5e0192517458
Hyunho Lee <77262005+lhh9799@users.noreply.github.com>
2024-04-21 19:34:57 +0900

    [Gold IV] Title: 감시, Time: 212 ms, Memory: 19900 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -5,6 +5,31 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
+/**
+ * <pre>
+ * 백준 15683번 감시
+ * https://www.acmicpc.net/problem/15683
+ * 
+ * 실행시간: 224ms (백준)
+ * 메모리: 20,476 KB
+ * 
+ * N×M 크기의 사무실에 총 K개의 CCTV가 설치되어있을 때, CCTV가 감시할 수 없는 영역 (사각지대)의 수를 구하는 문제입니다.
+ * 1번 CCTV는 1개의 방향
+ * 2번 CCTV는 2개의 방향
+ * 3번 CCTV는 2개의 방향
+ * 4번 CCTV는 3개의 방향
+ * 5번 CCTV는 4개의 방향
+ * 을 감시할 수 있으며 각 CCTV는 90도 회전하여 가로 또는 세로 방향으로 가능합니다.
+ * 
+ * 1. 사무실의 정보를 입력받으면서 CCTV의 좌표를 ArrayList에 저장합니다.
+ * 2. CCTV의 수 만큼 순열을 만듭니다. 단, 각 자리의 최댓값은 CCTV의 번호에 따라 달라집니다. (1번: 3, 2번: 1, 3번: 3, 4번: 3, 5번: 0) #4 - (CCTV가 한 번에 감시하는 방향의 수 - 1) - 1개
+ * 3. 순열이 완성되면 사각지대의 수를 구합니다.
+ * 
+ * </pre>
+ * 
+ *  @author 이현호
+ */
+
 public class Main {
 	
 	static int N, M;
@@ -12,7 +37,7 @@ public class Main {
 	static int[][] simulatedOffice;
 	static ArrayList<int[]> cctvList = new ArrayList<>();
 	static int answer = Integer.MAX_VALUE;
-	static int[] directions;
+	static int[] directions;			//CCTV가 감시하는 방향을 저장하는 순열
 	static int numberOfCctv;
 	static HashMap<Integer, Integer> possibleDirections = new HashMap<>();
 	
@@ -70,6 +95,7 @@ public class Main {
 		}
 	}
 	
+	//벽의 정보를 저장하기 위해 원본 사무실 배열을 덮어씀
 	static void initSimulatedOffice() {
 		for(int i=0; i<N; i++) {
 			System.arraycopy(office[i], 0, simulatedOffice[i], 0, office[i].length);
@@ -77,8 +103,8 @@ public class Main {
 	}
 	
 	static void permutation(int depth) {
+		//재귀 종료 조건
 		if(depth == numberOfCctv) {
-			//계산
 			initSimulatedOffice();
 			
 			for(int i=0; i<numberOfCctv; i++) {
@@ -87,7 +113,6 @@ public class Main {
 				int y = coord[1];
 				int cctvType = office[x][y];
 				int direction = directions[i];
-				simulatedOffice[x][y] = cctvType;	//CCTV가 차지하는 공간 초기화
 				
 				if(cctvType == 1) {
 					//1. direction == 0 -> 우
@@ -171,12 +196,13 @@ public class Main {
 				}
 			}
 			
-			int blindSpot = blindSpotCounter();
-			answer = Math.min(answer, blindSpot);
+			int blindSpot = blindSpotCounter();		//사각지대의 수 계산
+			answer = Math.min(answer, blindSpot);	//정답 갱신
 			
 			return;
 		}
 		
+		//재귀
 		int[] coord = cctvList.get(depth);
 		int cctvType = office[coord[0]][coord[1]];
 		int maxDirectionCount = possibleDirections.get(cctvType);
@@ -198,6 +224,7 @@ public class Main {
 		office = new int[N][M];
 		simulatedOffice = new int[N][M];
 		
+		//순열을 만들 때 한 자리의 최댓값
 		possibleDirections.put(1, 4);
 		possibleDirections.put(2, 2);
 		possibleDirections.put(3, 4);
@@ -210,16 +237,17 @@ public class Main {
 			for(int j=0; j<M; j++) {
 				office[i][j] = Integer.parseInt(st.nextToken());
 				
+				//CCTV 좌표를 ArrayList에 저장
 				if(office[i][j] >= 1 && office[i][j] <= 5) {
 					cctvList.add(new int[] {i, j});
 				}
 			}
 		}
 		
-		directions = new int[cctvList.size()];
+		directions = new int[cctvList.size()];	//CCTV가 감시하는 방향을 저장하는 순열 할당
 		numberOfCctv = cctvList.size();
 		
-		permutation(0);
+		permutation(0);							//순열 생성
 		
 		System.out.println(answer);
 	}
