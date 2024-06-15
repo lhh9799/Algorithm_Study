c015c61ef5b2d359b735485d4e20611d37e841e9
Hyunho Lee <77262005+lhh9799@users.noreply.github.com>
2024-06-15 16:34:36 +0900

    [Silver I] Title: 행렬, Time: 80 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Silver/1080. 행렬/행렬.java
+++ 백준/Silver/1080. 행렬/행렬.java
@@ -2,6 +2,30 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * <pre>
+ * 백준 1080번 행렬
+ * https://www.acmicpc.net/problem/1080
+ * 
+ * 실행시간: 84ms (백준)
+ * 메모리: 11,620 KB
+ * 
+ * 그리디 문제입니다.
+ * 
+ * <q>현재 뒤집는 시도가 이전에 뒤집어 맞춰놓은 배열에 영향을 끼쳐서는 안된다.</q>
+ * <cite>https://dev-minjeong.tistory.com/23</cite>
+ * 
+ * <q>왜 이렇게 하는것이 최소의 연산 횟수가 나오는지 의문이 생깁니다. 이유는, 3*3 행렬이 이동하면서, 이미 뒤집은 요소는 다시는 뒤집지 않기에 최소 연산횟수가 나옵니다.</q>
+ * <cite>https://passionfruit200.tistory.com/m/644</cite>
+ * 
+ * (0, 0)부터 (N-2, M-2)까지 값을 비교해 일치하지 않으면 뒤집습니다. (왼쪽 위 (i, j)를 기준으로 3×3 크기의 행렬을 뒤집기 때문에 -2까지를 범위로 함)
+ * 두 행렬이 일치하면 뒤집은 횟수를, 그렇지 않으면 -1을 출력합니다. 
+ * 
+ * </pre>
+ * 
+ *  @author 이현호
+ */
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
@@ -33,7 +57,7 @@ public class Main {
 			}
 		}
 		
-		//왼쪽 위 (i, j)를 기준으로 하기 때문에 루프는 -2 (i < N-2; j < M-2)
+		//왼쪽 위 (i, j)를 기준으로 3×3 크기의 행렬을 뒤집기 때문에 루프는 -2 (i < N-2; j < M-2)
 		for(int i=0; i<N-2; i++) {
 			for(int j=0; j<M-2; j++) {
 				if(A[i][j] != B[i][j]) {
